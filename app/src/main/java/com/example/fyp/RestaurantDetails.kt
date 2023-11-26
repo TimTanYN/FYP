db04b430d1e83a59b41596f70a4d843502b5c34f
TimTan1234 <102011075+TimTan1234@users.noreply.github.com>
2023-11-26 18:46:24 +0800

    26/11/2023

--- app/src/main/java/com/example/fyp/RestaurantDetails.kt
+++ app/src/main/java/com/example/fyp/RestaurantDetails.kt
@@ -1,10 +1,13 @@
 package com.example.fyp
 
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.fyp.adapter.ContractCard
 import com.example.fyp.adapter.Restaurant
 import com.example.fyp.adapter.restaurant_review_adapter
@@ -16,18 +19,24 @@ class RestaurantDetails : AppCompatActivity() {
         setContentView(R.layout.restaurant_details)
 
         val restaurant = intent.getSerializableExtra("RESTAURANT_DETAILS") as? Restaurant
-       val restaurantAddress = findViewById<TextView>(R.id.restaurantAddress)
+
+        val restaurantImage = findViewById<ImageView>(R.id.restaurantImage)
+        val restaurantAddress = findViewById<TextView>(R.id.restaurantAddress)
+        val priceRange = findViewById<TextView>(R.id.restaurantPriceRange)
+        val rating = findViewById<TextView>(R.id.rating)
         if (restaurant != null) {
+            Glide.with(restaurantImage.context).load(restaurant.photoUrl).into(restaurantImage)
             restaurantAddress.text = restaurant.address
+            priceRange.text = restaurant.priceRange.toString()
+            rating.text = restaurant.rating.toString()
         }
+
         var list: MutableList<Review> = mutableListOf()
         if (restaurant != null) {
             for (review in restaurant.reviews) {
                 list.add(Review(review.authorName, review.rating, review.text))
             }
         }
-
-
         val recyclerView = findViewById<RecyclerView>(R.id.restaurantComment)
         recyclerView.layoutManager = LinearLayoutManager(this) // Assuming 'this' is a Context
         recyclerView.adapter = restaurant_review_adapter(list)
--- app/src/main/java/com/example/fyp/adapter/restaurant_review_adapter.kt
+++ app/src/main/java/com/example/fyp/adapter/restaurant_review_adapter.kt
@@ -1,5 +1,6 @@
 package com.example.fyp.adapter
 
+import android.text.method.ScrollingMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,7 +16,7 @@ class restaurant_review_adapter (private val reviewList: List<Review>) : Recycle
     class ReviewViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val usernameTextView: TextView = view.findViewById(R.id.username)
         val profilePictureImageView: ImageView = view.findViewById(R.id.profilePicture)
-        val textTextView: TextView = view.findViewById(R.id.text)
+        var textTextView: TextView = view.findViewById(R.id.text)
         val ratingTextView: TextView = view.findViewById(R.id.rating)
     }
 
@@ -31,6 +32,10 @@ class restaurant_review_adapter (private val reviewList: List<Review>) : Recycle
         val formattedReview = formatReviewText(review.text)
         println("Formatted review text: $formattedReview") // Check formatted text
         holder.ratingTextView.text = review.rating.toString()
+        holder.textTextView.setOnClickListener {
+            // Toggle between expanded and collapsed state
+            holder.textTextView.maxLines = if (holder.textTextView.maxLines == 3) Integer.MAX_VALUE else 3
+        }
 
         // Set an image for profilePictureImageView if available
         // For example, using a placeholder image:
