203e2897fe98b3569938b89157e569c329214c17
JXJ19 <jongxj-wm20@student.tarc.edu.my>
2023-12-01 20:25:54 +0800

    JXJ

---
+++ app/src/main/java/com/example/fyp/activity/ManageAccommodationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.fyp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fyp.R
+
+class ManageAccommodationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manage_accommodation)
+    }
+}
